6c67e4f9dc1c0278cf370669fca3f120618fa546
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-12-01 16:18:29 +0200

    improved the mobile menu js

--- src/JS/mobile-menu.js
+++ src/JS/mobile-menu.js
@@ -4,7 +4,22 @@ export const toggleMobileMenu = () => {
     const { buttonRef, menuRef } = ref;
   
     buttonRef.addEventListener("click", () => {
-      menuRef.classList.toggle("is-open");
-      buttonRef.classList.toggle("is-open");
+      const isMenuOpen =
+      buttonRef.getAttribute('aria-expanded') === 'true' || false;
+      buttonRef.setAttribute('aria-expanded', !isMenuOpen);
+      menuRef.classList.toggle('is-open');
+
+    const scrollLockMethod = !isMenuOpen
+    ? 'disableBodyScroll'
+    : 'enableBodyScroll';
+ 
+  buttonRef.addEventListener('click', toggleMobileMenu);
+
+      window.matchMedia('(min-width: 835px)').addEventListener('change', e => {
+        if (!e.matches) return;
+        menuRef.classList.remove('is-open');
+        openMenuBtn.setAttribute('aria-expanded', false);
+        bodyScrollLock[scrollLockMethod](document.body);
+      });
     });
   };
